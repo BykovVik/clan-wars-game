df0aec154175821610f35d7b3a3b62826920380e
BykovVik <piton20091986@gmail.com>
2023-12-24 01:15:06 +0300

    add models

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from telethon import TelegramClient, events
 from dotenv import load_dotenv
 import os
+from models import create_all_tables
 
 load_dotenv()
 bot = TelegramClient('bot', os.getenv("API_ID"), os.getenv("API_HASH")).start(bot_token=os.getenv("TOKEN"))
@@ -9,5 +10,11 @@ bot = TelegramClient('bot', os.getenv("API_ID"), os.getenv("API_HASH")).start(bo
 @bot.on(events.NewMessage(pattern='/start'))
 async def start_command(event):
     await event.reply('Hello world')
+
+try:
+    create_all_tables()
+    print("Tables created successfully")
+except Exception as e:
+    print(e)
 
 bot.run_until_disconnected()
--- src/models.py
+++ src/models.py
@@ -1,1 +1,51 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import DeclarativeBase, relationship
 
+engine = create_engine('postgresql://admin:admin@localhost/tg_game')
+
+class Base(DeclarativeBase): pass
+
+class User(Base):
+    """
+    Сreating a users table in the database
+    """
+
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    user_id = Column(Integer)
+    score = Column(Integer)
+    penalties = Column(Integer)
+    rating = Column(Integer)
+    clan_id = Column(Integer, ForeignKey("clans.id"))
+    clan = relationship("Clan", back_populates="users")
+
+class Clan(Base):
+    """
+    Сreating a clans table in the database
+    """
+
+    __tablename__ = "clans"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    users = relationship("User", back_populates="clan")
+    score = Column(Integer)
+    rating = Column(Integer)
+
+def get_session():
+    """
+    Returns a session for working with the database
+    """
+
+    Session = sessionmaker(bind=engine)
+    return Session()
+
+def create_all_tables():
+    """
+    Creates all tables defined in models
+    """
+
+    Base.metadata.create_all(engine)
