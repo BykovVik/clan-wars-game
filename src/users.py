d1d4cef6014e83f5df4a87ff846ed835a584711d
BykovVik <piton20091986@gmail.com>
2023-12-24 17:59:53 +0300

    fix user controller

--- src/users.py
+++ src/users.py
@@ -1,11 +1,12 @@
 from .models import get_session, User
 
-Session = get_session()
-
 class UserController:
     """
     CRUD operations for the user table
     """
+    def __init__(self, user_id):
+        self.session = get_session()
+        self.user_id = user_id
     
     def add_user(self, name, user_id, chat_id):
         player = User(
@@ -16,24 +17,39 @@ class UserController:
             rating = 0,
             clan_id = chat_id
         )
-        Session.add(player)
-        Session.commit()
+        self.session.add(player)
+        self.session.commit()
 
-    def get_user(self, user_id):
-        user = Session.query(User).filter_by(user_id=user_id)
+    def get_user(self):
+        user = self.session.query(User).filter_by(user_id=self.user_id).first()
         return user
     
-    def update_user(self, name, user_id, score, penalties, rating, chat_id):
-        user = Session.query(User).filter_by(user_id=user_id)
-        user.name = name,
-        user.user_id = user_id,
-        user.score = score,
-        user.penalties = penalties,
-        user.rating = rating,
+    def update_user_name(self, name):
+        user = self.session.query(User).filter_by(user_id=self.user_id).first()
+        user.name = name
+        self.session.commit()
+
+    def update_user_score(self, score):
+        user = self.session.query(User).filter_by(user_id=self.user_id).first()
+        user.score = score
+        self.session.commit()
+
+    def update_user_penalties(self, penalties):
+        user = self.session.query(User).filter_by(user_id=self.user_id).first()
+        user.penalties = penalties
+        self.session.commit()
+
+    def update_user_rating(self, rating):
+        user = self.session.query(User).filter_by(user_id=self.user_id).first()
+        user.rating = rating
+        self.session.commit()
+
+    def update_user_clan_id(self, chat_id):
+        user = self.session.query(User).filter_by(user_id=self.user_id).first()
         user.clan_id = chat_id
-        Session.commit()
+        self.session.commit()
 
     def delete_user(self, user_id):
-        user = Session.query(User).filter_by(user_id=user_id)
-        Session.delete(user)
-        Session.commit()
+        user = self.session.query(User).filter_by(user_id=self.user_id).first()
+        self.session.delete(user)
+        self.session.commit()
