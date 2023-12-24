63ea5ae908e8d2ad1752ffc80c7b69232b0c1af5
BykovVik <piton20091986@gmail.com>
2023-12-24 21:23:58 +0300

    add clan registration

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
-from telethon import TelegramClient, events
+from telethon import TelegramClient, events, Button
 from dotenv import load_dotenv
 import os
 from models import create_all_tables
+from clans import ClanController
+from telethon.tl.functions.messages import DeleteMessagesRequest
 
 load_dotenv()
 bot = TelegramClient('bot', os.getenv("API_ID"), os.getenv("API_HASH")).start(bot_token=os.getenv("TOKEN"))
@@ -9,10 +11,57 @@ bot = TelegramClient('bot', os.getenv("API_ID"), os.getenv("API_HASH")).start(bo
 
 @bot.on(events.NewMessage(pattern='/start'))
 async def start_command(event):
-    
-    if not event.is_private:
-        await event.reply('Это первый бот для социальной игры в Телеграм!\n" "\n" "Специально для тебя мы прикрутили лайки❤️, дали возможность стать лидером клана👑 и бросить вызов любому чату⚔️ в Телеграме 👊" "\n" "\n" "Интересно? Чтобы начать, ДОБАВЬ меня в любой ЧАТ и попроси дать мне права АДМИНИСТРАТОРА. " "\n" "\n" "Создай свою тусовку в телеграме и стань первым!👊🏻 ')
 
+    if event.is_private:
+        return
+
+    controller = ClanController(event.chat.id)
+    clan = controller.get_clan()
+
+    if clan is None:
+        await event.respond(
+            "Это первый бот для социальной игры в Телеграм!\n\nСпециально для тебя, мы дали возможность бросить вызов любому чату⚔️ в Телеграме 👊\n\nИнтересно? Чтобы начать, ДОБАВЬ меня в любой ЧАТ и попроси дать мне права АДМИНИСТРАТОРА.\n\n Создай свою тусовку в телеграме и стань первым!👊🏻 ",
+            buttons = [
+                [Button.inline('Создать свой КЛАН', b'add_clan')]
+            ]
+        )
+    else:
+        await event.reply('Клан уже создан')
+
+
+@bot.on(events.NewMessage(pattern='/help'))
+async def help_command(event):
+
+    if event.is_private:
+        return
+    
+    controller = ClanController(event.chat.id)
+    clan = controller.get_clan()
+
+    if clan is None:
+        await event.reply("В вашем чате НЕ создан клан, нажмите команду `/start` и следуйте дальнейшим инструкциям")
+    else:
+        await event.reply("Вспомогательная информация")
+
+@bot.on(events.CallbackQuery)
+async def callback_answers(event):
+
+    #delete button afyer click
+    await bot.delete_messages(event.chat.id, event.message_id)
+
+    #add clan query
+    if event.data == b'add_clan':
+        controller = ClanController(event.chat.id)
+        controller.add_clan(
+            title = event.chat.title,
+            chat_id = event.chat.id,
+            wins = 0,
+            losses = 0,
+            rating = 0
+        )
+        await event.answer("Клан вашего чата успешно создан")
+
+#check database connect
 try:
     create_all_tables()
     print("Tables created successfully")
