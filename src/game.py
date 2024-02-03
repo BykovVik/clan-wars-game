8eb05845106d833b91cab8c7f86c0d5c8a64a9c0
BykovVik <piton20091986@gmail.com>
2024-02-04 01:09:07 +0300

    add code description

--- src/game.py
+++ src/game.py
@@ -16,26 +16,31 @@ class GameController:
         self.d_clan = self.d_clan_controller.get_clan()
 
     async def start_game(self):
-
+        # creating a message for the attacking clan
         a_chat_message = await self.bot.send_message(
             self.a_clan.chat_id,
             "Лукасай этот пост для того что б обыграть своих опонетов АТАКА"
         )
+        # creating a message for the protecting clan
         d_chat_message = await self.bot.send_message(
             self.d_clan.chat_id,
             "Лукасай этот пост для того что б обыграть своих опонетов"
         )
-
+        # add timer
         await asyncio.sleep(10)
-        # http://localhost:8000/messages/-1001414907164/34193
-        a_url = f"http://localhost:8000/messages/{self.a_clan.chat_id}/{a_chat_message.id}"
-        d_url = f"http://localhost:8000/messages/{self.d_clan.chat_id}/{d_chat_message.id}"
+        # creaye URLs
+        a_url = "http://localhost:8000/messages/{}/{}".format(self.a_clan.chat_id, a_chat_message.id)
+        d_url = "http://localhost:8000/messages/{}/{}".format(self.d_clan.chat_id, d_chat_message.id)
+        # get reactions objects
         a_count = requests.get(a_url)
         b_count = requests.get(d_url)
+        # get reactions count
         a = json.loads(a_count.text)['reactions_count']
         b = json.loads(b_count.text)['reactions_count']
+        # end game content
         await self.end_game(a_chat_message.id, d_chat_message.id)
 
     async def end_game(self, a_id, d_id):
+        # delete game messages
         await self.bot.delete_messages(self.a_clan.chat_id, a_id)
         await self.bot.delete_messages(self.d_clan.chat_id, d_id)
